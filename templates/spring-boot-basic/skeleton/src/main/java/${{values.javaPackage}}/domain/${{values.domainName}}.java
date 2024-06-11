6a89ed15b02da43fd08127ed57bdaa4a72621d6a
piomin <piotr.minkowski@gmail.com>
2024-06-12 00:31:13 +0200

    Adding new template

---
+++ templates/spring-boot-basic/skeleton/src/main/java/${{values.javaPackage}}/domain/${{values.domainName}}.java
@@ -0,0 +1,14 @@
+package ${{ values.groupId }}.domain;
+
+public class ${{ values.domainName }} {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
